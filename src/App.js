dd3c83ff594d00695068a9e59f1c5ed9458d44d0
mario-cunetta <60383719+mario-cunetta@users.noreply.github.com>
2020-01-28 18:10:57 +0100

    logo
    Inserito un logo di test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React , {Component} from 'react';
-//import logo from './logo.svg';
+import React, { Component } from "react";
+import logo from "./logotest.png";
 
-import './App.css';
-import Clock from './Clock';
+import "./App.css";
+import Clock from "./Clock";
 
-const Prima = <h2>Questa è la prima App</h2>
+const Prima = <h2>Questa è la prima App</h2>;
 
 class App extends Component {
-  
   render() {
-     return(
+    return (
       <React.Fragment>
-      <div className='App'>
-      
-         </div>
-        
-         {Prima}
-         <p>Questa è un'espressione che restituisce: {4+4}</p>
-         
-         <Clock timezone="0" country="Italia" />
-         <Clock timezone="2" country="Russia"/>
-         <Clock timezone="-6" country="Cuba"/>
-         
-         </React.Fragment>
-     );
+        <div className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+
+        {Prima}
+        <p>Questa è un'espressione che restituisce: {4 + 4}</p>
+
+        <Clock timezone="0" country="Italia" />
+        <Clock timezone="2" country="Russia" />
+        <Clock timezone="-6" country="Cuba" />
+      </React.Fragment>
+    );
   }
- 
 }
 
-  
 export default App;
